ce839c388851ff2c752ca24f85f22adc61ce67c7
SR <shashankr.blr@gmail.com>
2017-08-13 14:46:22 +0300

    Update highlightExtracter.py

--- SRSProject/highlightExtracter.py
+++ SRSProject/highlightExtracter.py
@@ -1,80 +1,90 @@
+import os
+from skimage.color import rgb2gray
+from skimage import data
+import matplotlib.pyplot as plt
+from skimage import io
+from skimage.viewer import ImageViewer
 import cv2
-import numpy as np 
-from matplotlib import pyplot as plt
+import base64
 
-#end with cv2.imwrite or something of that nature
+# in NumPy indexing, the first dimension (camera.shape[0]) corresponds to rows, while the second (camera.shape[1])
+#  corresponds to columns, with the origin (camera[0, 0]) on the top-left corner. 
 
+# hypothesis: the mean value is the value of the highlight
+# Anything between the two extremes counts
 
-def KeepHighlightsOnly(img, r, g, b): #convert r, g, b to intensities? 0 is black
-  channels = img.shape[2]
-  nCols = img.shape[1]*channels #Y
-  nRows = img.shape[0] #X
-  
-  count = 0
-  row = 0
+filename = 'images/medium.png'
+medium_image = io.imread(filename)
+gray_image = cv2.cvtColor(medium_image, cv2.COLOR_BGR2GRAY)
 
-  while (row < nRows):
-    p = img.item(row, 0, 0)
+# Gets a grayscale image as input
+
+def KeepHighlights(img):
+    nRows = img.shape[0] #Number of rows
+    nCols = img.shape[1] # opposite to cartesian coordinates
     count = 0
+    row = 0
+    print ("number of rows: ", nRows)
+    print ("number of cols: ", nCols)
+    while row < nRows:    
+        print(row)
+        p = img[row, 0] #beginning of row
+        count = 0
+        lowerValue = 150 #The (highest) shade of black - most light
+        higherValue = 255 #White
 
-    for i in xrange(0, nCols-3, 3):
-      if (img[row, i, 0] == b and img[row, i, 1] == g and img[row, i, 2] == r):
-        count += 1
-    
-    # we are going row by row
+        if p > lowerValue and p < higherValue:
+            withinRange = True
+        else:
+            withinRange = False
 
-    if count == 0:
-      for i in xrange(0, nCols, 1):
-        img.itemset((row, i, 0), 255) #doesn't make sense
+        # count the number of highlighted pixels in a row
 
-    else: 
-      start = row
-    
-    while count > 0:
-      count = 0
-      #same thing all over again
-      #we want to find another row without highlights tho
+        for i in xrange(0, nCols, 1):
+            if (img[row, i] > lowerValue and img[row, i] < higherValue):
+                count += 1
 
-      if row + 1 < nRows:
+        if count == 0:
+            for i in xrange(0, nCols, 1):
+                img[row, i] = 255
+        else:
+            start = row
+
+        while count > 0:
+            count = 0       
+            if (row + 1) < nRows:
+                row += 1
+                p = img[row, 0]
+
+                for i in xrange(0, nCols, 1):
+                    
+                    if (img[row, i] > lowerValue and img[row, i] < higherValue):
+                        count += 1
+            
+            if count == 0:
+                row -= 1 # every row on top of this is gonna become white
+                end = row
+                p = img[end, 0]
+
+                for i in xrange(0, nCols, 1):
+                    for j in xrange(0, end, 1):
+                        p = img[j, i]
+                        p = 255
         row += 1
-        p = img[row, :, :]
 
-        for i in xrange(0, nCols-3, 3):
-          if (img[row, i, 0] == b and img[row, i, 1] == g and img[row, i, 2] == r):
-            count += 1
+    for row in xrange(1, nRows, 1):
+        p = img[row, 0]       
+        print ("remove all highlighting - row number ", row)
+        for i in xrange(0, nCols, 1):
+            if withinRange:
+                p = 255
 
-      if count == 0:
-        row -= 1
-        end = row
-        p = img.item(end, 0, 0)
+    viewer = ImageViewer(img)
+    viewer.show()
+    print("Viewer.show() has been implemented, over!")
 
-        for i in xrange(0, nCols-3, 3):
-          if (img[row, i, 0] != b and img[row, i, 1] != g and img[row, i, 2] != r):
-            for j in xrange(start+1, end+1, 1):
-              p = img.item(row, j, 0)
-              img.itemset((row, j, 0), 255)
-              img.itemset((row, j, 1), 255)
-              img.itemset((row, j, 2), 255)
-    row += 1 
+KeepHighlights(gray_image)
 
 
-  #remove all the highlighting, leaving only the text that was initially highlighted
-  for row in xrange(1, nRows, 1):
-    p = img.item(row, 0, 0)
-
-    for i in xrange(0, nCols-3, 3):
-      if img[row, i, 0] == b and img[row, i, 1] == g and img[row, i, 2] == r:
-        img.itemset((row, j, 0), 255)
-        img.itemset((row, j, 1), 255)
-        img.itemset((row, j, 2), 255)
-
-  img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-  plt.subplot(231),plt.imshow(img,'gray'),plt.title('not-ORIGINAL')
-  print("What just happened?")
-  plt.show()
-
-myimage = cv2.imread('images/highlight_ocr.png')
-
-KeepHighlightsOnly(myimage, 251, 226, 152)
+# very handy viewer = ImageViewer(gray_image)
+# viewer.show()
